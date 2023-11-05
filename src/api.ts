2bcdc468f8cffe8142738512aaa31fb035ebfa5d
dLevz <100078678+danlevison@users.noreply.github.com>
2023-11-05 13:44:44 +0000

    feat: implement data handling for van details page

--- src/api.ts
+++ src/api.ts
@@ -4,3 +4,8 @@ export async function fetchVansData() {
 	const { data } = await supabase.from("vans").select()
 	return data
 }
+
+export async function fetchVanData(id: string) {
+	const { data } = await supabase.from("vans").select().eq("vanId", id)
+	return data
+}
--- src/pages/Vans/VanDetail.tsx
+++ src/pages/Vans/VanDetail.tsx
@@ -1,22 +1,25 @@
 import { useContext, useEffect, useState } from "react"
 import VansDataContext from "../../context/VansDataContext"
+import { fetchVanData } from "@/api"
 import { useParams, Link } from "react-router-dom"
 import { BsArrowLeft } from "react-icons/bs"
 import Overview from "../../components/vanDetail/Overview"
 import ImageHeader from "../../components/vanDetail/ImageHeader"
-//types
-import { VanType } from "../../types/vanType"
 import Description from "../../components/vanDetail/Description"
 import Features from "../../components/vanDetail/Features"
 import Details from "../../components/vanDetail/Details"
 import Rules from "../../components/vanDetail/Rules"
 import CheckAvailability from "../../components/vanDetail/CheckAvailability"
 import Cancellation from "../../components/vanDetail/Cancellation"
+//types
+import { VanType } from "../../types/vanType"
 
 export default function VanDetail() {
 	const { vans } = useContext(VansDataContext)
 	const [van, setVan] = useState<VanType | null>(null)
 	const { id } = useParams()
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState<string | null>(null)
 
 	const vanTypeColour = {
 		simple: "bg-[#E17654]",
@@ -24,16 +27,56 @@ export default function VanDetail() {
 		luxury: "bg-[#161616]"
 	} as Record<string, string>
 
-	const getVan = (vanId: string) => {
-		const vanDetail = vans?.find((van) => van.vanId === vanId)
-		setVan(vanDetail ?? null)
-	}
-
 	useEffect(() => {
 		if (id) {
+			const getVan = (vanId: string) => {
+				const vanDetail = vans?.find((van) => van.vanId === vanId)
+				setVan(vanDetail ?? null)
+			}
 			getVan(id)
 		}
-	}, [])
+	}, [id, vans])
+
+	useEffect(() => {
+		if (!vans && id) {
+			const loadVan = async () => {
+				setLoading(true)
+				setError(null)
+				try {
+					const data = await fetchVanData(id)
+					data?.map((van) => setVan(van))
+				} catch (error) {
+					console.log(error)
+					setError("It looks like something went wrong!")
+				} finally {
+					setLoading(false)
+				}
+			}
+			loadVan()
+		}
+	}, [id, vans])
+
+	if (loading) {
+		return (
+			<div className="min-h-screen py-32">
+				<h1 className="text-center text-4xl">Loading...</h1>
+			</div>
+		)
+	}
+
+	if (error) {
+		return (
+			<div className="flex flex-col items-center min-h-screen py-32">
+				<h1 className="text-center text-4xl pb-3">{error}</h1>
+				<Link
+					to={"/vans"}
+					className="px-3 py-2 bg-[#161616] text-white rounded-lg font-semibold"
+				>
+					Please try again
+				</Link>
+			</div>
+		)
+	}
 
 	return (
 		<div className="min-h-screen mx-auto px-8 py-20">
