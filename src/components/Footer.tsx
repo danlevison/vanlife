c7e7e8b98a151997da74b13b0268c46087a5118d
dLevz <100078678+danlevison@users.noreply.github.com>
2023-11-09 11:02:02 +0000

    feat: FAQ's accordion in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,13 @@ import { Link } from "react-router-dom"
 import { CiLock } from "react-icons/ci"
 import { AiOutlineFacebook, AiOutlineInstagram } from "react-icons/ai"
 import { RiTwitterXLine } from "react-icons/ri"
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger
+} from "@/components/ui/accordion"
+
 import visa from "@/assets/visa.png"
 import mastercard from "@/assets/mastercard.png"
 import amex from "@/assets/amex.png"
@@ -12,7 +19,7 @@ export default function Footer() {
 	return (
 		<footer className="bg-background w-full">
 			<div className="w-full max-w-[1440px] mx-auto px-6 sm:px-8">
-				<div className="flex flex-col md:flex-row justify-between gap-8 pt-16 md:pt-28 pb-10">
+				<div className="flex flex-col md:flex-row justify-between gap-10 md:gap-16 pt-16 md:pt-28 pb-10">
 					<div className="text-primaryText">
 						<h3 className="text-2xl font-bold uppercase">#Vanlife</h3>
 						<ul className="flex flex-col gap-4 mt-7">
@@ -50,22 +57,48 @@ export default function Footer() {
 							</li>
 						</ul>
 					</div>
-					<div>
+					<div className="w-full">
 						<h3 className="text-2xl font-bold">FAQ's</h3>
-						<ul className="flex flex-col gap-4 mt-7">
-							<li className="cursor-pointer hover:text-accent duration-300">
-								How renting a campervan works
-							</li>
-							<li className="cursor-pointer hover:text-accent duration-300">
-								Can I get a discount for last-minute hire?
-							</li>
-							<li className="cursor-pointer hover:text-accent duration-300">
-								What if I need to cancel?
-							</li>
-							<li className="cursor-pointer hover:text-accent duration-300">
-								More FAQ's
-							</li>
-						</ul>
+						<Accordion
+							type="single"
+							collapsible
+							className="w-full"
+						>
+							<AccordionItem value="item-1">
+								<AccordionTrigger className="text-left">
+									Can I get a discount for last-minute hire?
+								</AccordionTrigger>
+								<AccordionContent>
+									Absolutely! We're all about spontaneous adventures. While we
+									can't promise last-minute discounts, we do have some great
+									deals for those who embrace the unexpected. Check out our
+									current promotions and hit the road with a smile!
+								</AccordionContent>
+							</AccordionItem>
+							<AccordionItem value="item-2">
+								<AccordionTrigger className="text-left">
+									What if I need to cancel?
+								</AccordionTrigger>
+								<AccordionContent>
+									Change of plans? No worries! If you cancel 20 or more days
+									before your pick-up, you get a full refund. For cancellations
+									72 hours to 20 days prior, choose between a 50% refund or a
+									travel voucher (full value with a 15% fee). Unfortunately,
+									within 72 hours, no refunds or vouchers. We appreciate your
+									understanding and flexibility!
+								</AccordionContent>
+							</AccordionItem>
+							<AccordionItem value="item-3">
+								<AccordionTrigger className="text-left">
+									Can I return my campervan to a different location?
+								</AccordionTrigger>
+								<AccordionContent>
+									Yes, we offer one-way rentals for those who like to mix up
+									their journey. Just let us know in advance, and we'll make
+									sure your road trip ends right where you want it to.
+								</AccordionContent>
+							</AccordionItem>
+						</Accordion>
 					</div>
 					<div className="max-w-[600px]">
 						<h3 className="text-primaryText text-2xl font-bold">Vanlife</h3>
