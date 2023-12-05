50019406899a51a4beff1640ebd0d7d4966e0258
dLevz <100078678+danlevison@users.noreply.github.com>
2023-12-05 13:38:56 +0000

    fix: remove unused useEffect to prevent build error

--- src/components/ai/Assistant.tsx
+++ src/components/ai/Assistant.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react"
+import { useState, useRef } from "react"
 import useAuth from "@/hooks/useAuth"
 import supabase from "@/config/supabaseClient"
 import { useLocation, useNavigate } from "react-router-dom"
